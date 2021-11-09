f442be82fb0654eebf95c7a33ebb461c576a2156
AndyN9 <13814584+AndyN9@users.noreply.github.com>
2021-11-09 16:29:50 -0600

    add annual salary getter and setter

--- php/401k-contribution/src/Contribution401k.php
+++ php/401k-contribution/src/Contribution401k.php
@@ -2,6 +2,22 @@
 
 namespace AndyN9\Contribution401kLibrary;
 
-class Contribution401k {
+use TypeError;
 
+class Contribution401k {
+  private $annualSalary;
+
+  public function getAnnualSalary() {
+
+    return $this->annualSalary;
+  }
+
+  public function setAnnualSalary($amount) {
+    if (!is_numeric($amount)) {
+
+      throw new TypeError('Annual salary amount needs to be a number');
+    }
+
+    $this->annualSalary = $amount;
+  }
 }
--- php/401k-contribution/test/Contribution401kTest.php
+++ php/401k-contribution/test/Contribution401kTest.php
@@ -9,4 +9,18 @@ class Contribution401kTest extends TestCase {
   protected function setUp(): void {
     $this->contribution401k = new Contribution401k();
   }
+
+  public function testAnnualSalaryValidationShouldError() {
+    $this->expectExceptionMessage('Annual salary amount needs to be a number');
+    $this->contribution401k->setAnnualSalary(true);
+    $this->contribution401k->setAnnualSalary('foo');
+  }
+
+  public function testAnnualSalaryValidationShouldSet() {
+    $this->contribution401k->setAnnualSalary(60000);
+    $this->assertEquals($this->contribution401k->getAnnualSalary(), 60000);
+
+    $this->contribution401k->setAnnualSalary(60000.50);
+    $this->assertEquals($this->contribution401k->getAnnualSalary(), 60000.50);
+  }
 }
